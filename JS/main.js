7e2e0134e241845d97594a67dab9bf7ffa5d8565
Maurilio Ayrton Teotonio Alves <maurilio.alves@mctic.gov.br>
2024-08-01 16:52:00 -0300

    enviado projeto para git hub

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+function toggleChildren(element) {
+    event.stopPropagation();
+    element.classList.toggle('expanded');
+}
+
+function openModal(modalId) {
+    event.stopPropagation();
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modals = document.getElementsByClassName("modal");
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
